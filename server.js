7886a39fce537b576a6cc6f9bd45fbb75e9c4891
minchoCoin <bigteach0508@pusan.ac.kr>
2023-05-16 19:52:45 +0900

    make submenu files
    make submenu files and linking

--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 //server(node.js) code
 //execute as 'node server.js'
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const express = require('express');
+const app = express();
 
-http.createServer(function(request,response){
-    var pathname = url.parse(request.url).pathname;
+app.use(express.static(__dirname + '/public'))
 
-    if(pathname='/'){
-        fs.readFile('main.html',function(error,data){
-            response.writeHead(200,{'Content-Type': 'text/html'});
-            response.end(data);
-        });
-    }
-}).listen(3000,function(){
-    console.log('Server Running at http://127.0.0.1:3000');
+app.listen('3000',()=>{
+    console.log('listen on 3000');
 });
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/main.html');
+})
+
+app.get('/beverage',(req,res)=>{
+    res.sendFile(__dirname + '/beverage.html');
+})
+
+app.get('/chef',(req,res)=>{
+    res.sendFile(__dirname + '/chef.html');
+})
+
+app.get('/contact',(req,res)=>{
+    res.sendFile(__dirname + '/contact.html');
+})
+
+app.get('/direction',(req,res)=>{
+    res.sendFile(__dirname + '/direction.html');
+})
+
+app.get('/dish',(req,res)=>{
+    res.sendFile(__dirname + '/dish.html');
+})
+
+app.get('/greeting',(req,res)=>{
+    res.sendFile(__dirname + '/greeting.html');
+})
+
+app.get('/reservation',(req,res)=>{
+    res.sendFile(__dirname + '/reservation.html');
+})
